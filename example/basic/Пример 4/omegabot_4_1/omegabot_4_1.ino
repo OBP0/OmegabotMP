e87f4de0c73456f8827fd703c5a93e742ae03e37
Boris Volkov <volkov.boria2003@mail.ru>
2025-02-11 21:29:53 +0300

    Обновлены примеры

--- example/basic/Пример 4/omegabot_4_1/omegabot_4_1.ino
+++ example/basic/Пример 4/omegabot_4_1/omegabot_4_1.ino
@@ -1,20 +1,20 @@
-#define IK_PIN A0 // Определяем к какому пину подключен ИК датчик
+#define IR_PIN A0 // Определяем к какому пину подключен ИК датчик
 
-int ReadIK(){   // Функция чтения данных с ИК дальномера
-  double measur  = analogRead(IK_PIN) + 1;    // Считывание показаний АЦП
-  double voltage = measur * 5 / 1023;         // Преобразование в вольты
-  double dist    = 29.988 * pow(voltage, -1.173);  // Преобразование в сантиметры
+float Read_IR(){   // Функция чтения данных с ИК дальномера
+  float measur  = analogRead(IR_PIN) + 1;    // Считывание показаний АЦП
+  float voltage = measur * 5 / 1023;         // Преобразование в вольты
+  float dist    = 29.988 * pow(voltage, -1.173);  // Преобразование в сантиметры
   return dist;
 }
 
 
 void setup() {
-  pinMode(IK_PIN, INPUT);
+  pinMode(IR_PIN, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  float S = ReadIK(); // Получение расстояния с датчика
+  float S = Read_IR(); // Получение расстояния с датчика
   Serial.println(S);  // Вывод данных в терминал
   delay(100);         // Задежка 100 мс
   // В данном примере чтение показаний происходит без фильтрации.
